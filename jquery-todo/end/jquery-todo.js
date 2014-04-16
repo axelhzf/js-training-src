0ef59d29b6fc0511b9db34aa27fc079c6f39067e
Axel Hernández Ferrera <axelhzf@gmail.com>
2014-04-16 19:22:24 +0100

    jquery-todo: logic

---
+++ jquery-todo/end/jquery-todo.js
@@ -0,0 +1,34 @@
+(function () {
+  "use strict";
+
+  var $form = $("form");
+  var $tbody = $(".todo-list");
+  var $input = $("input[type=text]");
+
+  function addTask (task) {
+    $tbody.append("<tr><td><input type='checkbox'></td><td>" + task + "</td><td><a class='delete' href='#'>Delete</a></td></tr>");
+  }
+
+  $form.on("submit", function (e) {
+    e.preventDefault();
+    var task = $input.val();
+    if (task.length) {
+      addTask(task);
+    }
+    $input.val("");
+  });
+
+  $tbody.on("change", "input", function (e) {
+    var $check = $(e.currentTarget);
+    var $tr = $check.parents("tr");
+    $tr.toggleClass("completed", $check.is(':checked'));
+  });
+
+  $tbody.on("click", ".delete", function (e) {
+    e.preventDefault();
+    var $tr = $(e.currentTarget).parents("tr");
+    $tr.remove();
+  });
+
+
+}());
